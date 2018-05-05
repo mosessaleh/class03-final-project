149c42e7cd18050694644e872d12cfd4d727d71a
Zaki Wasik <zaki@webconsu.lt>
2018-05-05 12:11:41 +0200

    Read database configuration from environment variables.

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,12 +1,17 @@
+// If we are not on a production server load the .env file
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
+
 // Update with your config settings.
 
 module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      database: 'hyf_dev',
-      user:     'test',
-      password: 'test'
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD
     },
     pool: {
       min: 2,
