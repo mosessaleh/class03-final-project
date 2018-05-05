53d4a307c851d0452a9211ce316bdf525ac8cc40
Zaki Wasik <zaki@webconsu.lt>
2018-05-05 12:41:59 +0200

    Refactor: we don't need the knexfile at all.

--- server/helpers/knex.js
+++ server/helpers/knex.js
@@ -1,3 +1,26 @@
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('../knexfile');
-module.exports = require('knex')(configuration);
+// Do not put your database credentials directly in this
+// file as it will be visible on github!
+// Check the readme-file for details.
+
+// If we are not on a production server load the .env file
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
+
+module.exports = require('knex')(
+  {
+    client: 'mysql',
+    connection: {
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+);
