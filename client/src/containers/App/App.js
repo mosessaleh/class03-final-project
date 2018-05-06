e4c3f3e49aaddb6f0993208394600345212cefbb
Mousa Saleh <mousasaleh@MBPalkhasbMousa.localdomain>
2018-05-06 14:02:55 +0200

    Test

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './../../styles/App.css';
 
+
+// Just some contents
 class App extends Component {
   render() {
     return (
