b977fb0f9a2aa858d837dc7e9abf7b0780920150
Mousa Saleh <mousasaleh@MBPalkhasbMousa.localdomain>
2018-05-06 14:34:10 +0200

    Change to categories

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -5,7 +5,7 @@ var knex  = require('../helpers/knex');
 
 /* GET category listing. */
 router.get('/', function(req, res, next) {
-  knex('category').select().then( function(data) {
+  knex('categories').select().then( function(data) {
     res.send(data);
   });
 });
